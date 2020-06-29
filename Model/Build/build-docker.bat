0898c0bc28a89beabd1d7c81f1c7555dca82d8d2
Drew Holzworth <drew.holzworth@csiro.au>
2020-06-29 11:25:27 +1000

    Added script to build and compile apsim via docker.

---
+++ Model/Build/build-docker.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enableDelayedExpansion
+
+if "%APSIM%"=="" (
+	pushd %~dp0..\..
+	set "APSIM=!cd!"
+	popd
+)
+
+docker build -t buildapsim %~dp0Docker.build.latest
+if errorlevel 1 exit /b 1
+
+docker run -m 6g --cpu-count %NUMBER_OF_PROCESSORS% -v "%APSIM%":/APSIM buildapsim
+
+endlocal
